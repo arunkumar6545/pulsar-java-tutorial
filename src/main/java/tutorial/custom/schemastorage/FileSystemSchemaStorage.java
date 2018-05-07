3710e2337c9e52da91b1517064c95ac463db38e0
Luc Perkins <lucperkins@gmail.com>
2018-05-07 13:57:52 -0700

    fix more merge conflicts

--- src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorage.java
+++ src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorage.java
@@ -1,14 +1,19 @@
 package tutorial.custom.schemastorage;
 
+import com.google.common.io.Files;
 import org.apache.pulsar.broker.service.schema.SchemaStorage;
 import org.apache.pulsar.broker.service.schema.StoredSchema;
 import org.apache.pulsar.common.schema.SchemaVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
 public class FileSystemSchemaStorage implements SchemaStorage {
-    private static final File rootDirectory;
+    private static final Logger LOG = LoggerFactory.getLogger(FileSystemSchemaStorage.class);
+
+    private final File rootDirectory;
 
     public FileSystemSchemaStorage() {
         rootDirectory = Files.createTempDir();
@@ -16,7 +21,7 @@ public class FileSystemSchemaStorage implements SchemaStorage {
 
     @Override
     public void start() {
-
+        LOG.info("Creating filesystem schema storage engine with a root directory of {}", rootDirectory.getAbsolutePath());
     }
 
     @Override
