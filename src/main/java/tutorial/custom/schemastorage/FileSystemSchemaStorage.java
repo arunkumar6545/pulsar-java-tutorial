684e8c620032320d39c54be00473f476e30e2aef
Luc Perkins <lucperkins@gmail.com>
2018-05-07 13:52:27 -0700

    add baseline implementations

--- src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorage.java
+++ src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorage.java
@@ -1,4 +1,44 @@
 package tutorial.custom.schemastorage;
 
-public class FileSystemSchemaStorage {
+import org.apache.pulsar.broker.service.schema.SchemaStorage;
+import org.apache.pulsar.broker.service.schema.StoredSchema;
+import org.apache.pulsar.common.schema.SchemaVersion;
+
+import java.io.File;
+import java.util.concurrent.CompletableFuture;
+
+public class FileSystemSchemaStorage implements SchemaStorage {
+    private static final File rootDirectory;
+
+    public FileSystemSchemaStorage() {
+        rootDirectory = Files.createTempDir();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public SchemaVersion versionFromBytes(byte[] bytes) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<SchemaVersion> delete(String key) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<StoredSchema> get(String key, SchemaVersion version) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<SchemaVersion> put(String key, byte[] value, byte[] hash) {
+        return null;
+    }
+
+    @Override
+    public void close() {}
 }
--- src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorageFactory.java
+++ src/main/java/tutorial/custom/schemastorage/FileSystemSchemaStorageFactory.java
@@ -1,4 +1,15 @@
 package tutorial.custom.schemastorage;
 
-public class FileSystemSchemaStorageFactory {
+import org.apache.pulsar.broker.PulsarService;
+import org.apache.pulsar.broker.service.schema.SchemaStorage;
+import org.apache.pulsar.broker.service.schema.SchemaStorageFactory;
+
+import javax.validation.constraints.NotNull;
+
+public class FileSystemSchemaStorageFactory implements SchemaStorageFactory {
+    @Override
+    @NotNull
+    public SchemaStorage create(PulsarService pulsarService) throws Exception {
+        return new FileSystemSchemaStorage();
+    }
 }
